73de7d177b836068dfbab946cd368efaaaa103a1
LarsGJobloop <lars.gunnar@jobloop.no>
2024-02-12 09:34:46 +0000

    add examples

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -7,6 +7,26 @@ export function HomePage() {
   return (
     <main>
       <h1>Home Page</h1>
+
+      <section>
+        <h2>Innline Event Handling</h2>
+        <InlineEventHandling />
+      </section>
+
+      <section>
+        <h2>Event Handling</h2>
+        <EventHandling />
+      </section>
+
+      <section>
+        <h2>Input Text</h2>
+        <InputText />
+      </section>
+
+      <section>
+        <h2>Mouse Tracker</h2>
+        <MouseTracker />
+      </section>
     </main>
   )
 }
