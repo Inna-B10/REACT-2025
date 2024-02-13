7785602829f9f40068d448bf10c2c20885855932
LarsGJobloop <lars.gunnar@jobloop.no>
2024-02-13 11:04:00 +0000

    add tag filtering example

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import { articleResponse } from "./posts";
 
+// Denne fungerer ikke som den skal
+const tags = articleResponse.articles
+  .reduce((tagList, article) => [...tagList, ...article.tagList], [])
+  .filter((item, i, array) => array.indexOf(item) === array.lastIndexOf(item))
 
 /**
  * @typedef {typeof articleResponse.articles[0]} Article
@@ -10,14 +14,26 @@ import { articleResponse } from "./posts";
 export function List() {
   // Tilstander vi trenger å holde rede på
   const [searchTerm, setSearchTerm] = useState("")
+  const [tagFilter, setTagFilter] = useState("at")
 
   // Deriverte tilstander (verdier/variabler vi kan beregne basert på det vi allered har)
   const filterdList = articleResponse.articles
+    .filter((article) => article.tagList.includes(tagFilter))
     .filter((article) => customArticleFilter(article, searchTerm.toLowerCase()))
 
   return (
     <div>
       <input type="text" onChange={(event) => setSearchTerm(event.target.value.toLowerCase())} />
+
+      <select onChange={(event) => setTagFilter(event.target.value)}>
+        {tags.map((tag) => {
+          return (
+            <option key={tag} value={tag}>
+              {tag}
+            </option>
+          )
+        })}
+      </select>
 
       <ul>
         {filterdList.map((article) => {
