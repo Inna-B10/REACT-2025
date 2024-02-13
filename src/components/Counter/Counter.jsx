91564d59a59cc28be4f08d5afc7a05241976c8d1
LarsGJobloop <lars.gunnar@jobloop.no>
2024-02-13 11:32:29 +0000

    add basic styling to counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -16,11 +16,15 @@ export function Counter() {
   }
 
   return (
-    <div>
-      <p>Count: {count}</p>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
-      <button onClick={reset}>Reset</button>
+    <div className='bg-neutral-200 px-2 pt-4 pb-2 rounded-lg shadow-md border border-neutral-100/20'>
+      <p className='flex w-24'>Count: <span className='font-semibold ml-auto overflow-hidden'>{count}</span></p>
+      <div className='flex flex-col gap-2'>
+        <div className='flex gap-1'>
+          <button className='grow font-extrabold bg-teal-300 hover:bg-teal-200 active:bg-teal-400 rounded-md border border-neutral-100/20' onClick={increment}>+</button>
+          <button className='grow font-extrabold bg-teal-300 hover:bg-teal-200 active:bg-teal-400 rounded-md border border-neutral-100/20' onClick={decrement}>-</button>
+        </div>
+        <button className='font-semibold bg-red-300 hover:bg-red-200 active:bg-red-400 rounded-md border border-neutral-100/20' onClick={reset}>Reset</button>
+      </div>
     </div>
   )
 }
