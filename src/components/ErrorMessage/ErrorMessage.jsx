1668a7272cce860c53cc487e2f135b8ac68a5394
LarsGJobloop <lars.gunnar@jobloop.no>
2024-02-15 13:38:54 +0000

    add error message component

---
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -0,0 +1,26 @@
+/**
+ * Displays an error messgae
+ * 
+ * @param {{
+ * message: string
+ * }} props 
+ */
+export function ErrorMessage(props) {
+  return (
+    <div>
+      <p>
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="red" height="1em">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+          </svg>
+        </span>
+        {props.message}
+        <span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="red" height="1em">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+          </svg>
+        </span>
+      </p>
+    </div>
+  )
+}
