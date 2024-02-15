8bdc1143058b0f47670fb50d25b5a62dfcaa4623
LarsGJobloop <lars.gunnar@jobloop.no>
2024-02-15 14:45:10 +0000

    add styling for the articles list

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useFetch } from "./hooks/useFetch";
 import {ArticleCard} from './components/ArticleCard/ArticleCard'
 import { LoadingSpinner } from "./components/LoadingSpinner/LoadingSpinner";
 import { ErrorMessage } from "./components/ErrorMessage/ErrorMessage";
+import style from './App.module.css'
 
 function App() {
   const { data, error, isLoading } = useFetch(
@@ -19,12 +20,12 @@ function App() {
         {enableTimer ? <Countdown time={10} /> : <p>Timer hidden</p>}
       </section>
 
-      <section>
+      <section className={style["section-articles"]}>
         <h2>Articles</h2>
         {isLoading ? (<LoadingSpinner />)
         : error ? (<ErrorMessage message="failed fetching data" />)
         : (
-          <ul>
+          <ul className={style["articles"]}>
             {data.articles.map((article) => {
               return (
                 <li key={article.slug}>
